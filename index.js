e518c6d4f4cfcfc28d3842e1e10883a882d9484b
Shibangi Das <shibangid319@gmail.com>
2023-02-19 13:24:03 +0530

    added Backend

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const express=require("express")
+const { connection } = require("./db")
+const { authenticate } = require("./middleware/Authentication.middleware")
+const { noteRouter } = require("./routes/Note.route")
+const { userRouter } = require("./routes/User.routes")
+const cors=require("cors")
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("HOme PAge")
+})
+
+app.use("/users",userRouter)
+
+app.use(authenticate)
+app.use("/notes",noteRouter)
+app.listen(7500,async()=>{
+    try {
+        await connection
+        console.log("Connected to DB");
+    } catch (error) {
+        console.log(" Cannot connected to DB");
+        console.log(error);
+    }
+    console.log("Running the server at port 7500");
+})
